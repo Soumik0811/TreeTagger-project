fbe4436147eed329b419b50c0e62824b3d5a0f2a
Soumik0811 <91133940+Soumik0811@users.noreply.github.com>
2025-02-07 20:50:16 +0530

    Update supabase.py

--- supabase.py
+++ supabase.py
@@ -1,8 +1,8 @@
 from supabase_py import create_client
 
 # Initialize Supabase client
-supabase_url = 'https://lhcpvlzksihcncnuoelx.supabase.co'
-supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoY3B2bHprc2loY25jbnVvZWx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyODE3MzcsImV4cCI6MjAyODg1NzczN30.SaD2nniti_XV-a24G7YWQy2dK37Rx4bJZ6TJga8Tj4U'
+supabase_url = 'supabase url'
+supabase_key = 'supabase key'
 supabase = create_client(supabase_url, supabase_key)
 
 
